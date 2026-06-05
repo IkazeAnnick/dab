d72d77bdb11ca96f4c8eee4e65ac9cb8ca7da1c5
IkazeAnnick <cikaze154@gmail.com>
2026-06-05 13:40:27 +0200

    removed password of db

--- backend/database.js
+++ backend/database.js
@@ -11,7 +11,7 @@ async function initDatabase() {
   pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '123',
+    password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'sms',
     port: Number(process.env.DB_PORT) || 3306,
     waitForConnections: true,
